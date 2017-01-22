df87474ca2b98cf8e9cacc9b7158432eb509709a
jysuh777 <jysuh@ucsd.edu>
2017-01-21 16:56:45 -0800

    Empty iOS App

---
+++ front-end/ios/EatBae/EatBae/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EatBae
+//
+//  Created by June Suh on 1/21/17.
+//  Copyright © 2017 EatBae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
