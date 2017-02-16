64492d78dbf9ac851d1794456445ecc2d3ff5c65
hqtran <hqtran@ucsd.edu>
2017-02-16 02:59:16 -0800

    Added write to file function

---
+++ front-end/android/app/src/main/java/ebaeapp/com/ebae/SavePreferenceAction.java
@@ -0,0 +1,12 @@
+package ebaeapp.com.ebae;
+
+/**
+ * Created by Hung on 2/15/2017.
+ * parse the information in settings Activity to make sure there are no
+ * errors in the settings. This mostly consists of entirely empty settings which
+ * would result in search failures
+ */
+
+public class SavePreferenceAction {
+
+}
