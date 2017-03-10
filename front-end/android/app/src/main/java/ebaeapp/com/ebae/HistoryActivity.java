b8919de83215f73a6493d80a5822b1ee58e3ff0d
hqtran <hqtran@ucsd.edu>
2017-03-09 18:01:04 -0800

    Adding stuff to history, altered manifest

--- front-end/android/app/src/main/java/ebaeapp/com/ebae/HistoryActivity.java
+++ front-end/android/app/src/main/java/ebaeapp/com/ebae/HistoryActivity.java
@@ -3,6 +3,7 @@ package ebaeapp.com.ebae;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -12,6 +13,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by thiba on 14/02/2017.
@@ -36,6 +38,12 @@ public class HistoryActivity extends AppCompatActivity {
       }
       bt.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
               LinearLayout.LayoutParams.WRAP_CONTENT));
+      JSONObject currButtonJson = businesses[buttonIndex];
+      bt.setOnClickListener(new View.OnClickListener() {
+          public void onClick(View v) {
+            startHistRestScreen(currButtonJson);
+          }
+      });
 
 
       linearLayout.addView(bt);
@@ -44,4 +52,15 @@ public class HistoryActivity extends AppCompatActivity {
     ButterKnife.bind(this);
     Intent intent = getIntent();
   }
+
+  private void startHistRestScreen(JSONObject passBusiness) {
+
+    Intent intent = new Intent(HistoryActivity.this, HistoryRestaurantActivity.class);
+    intent.putExtra("business", passBusiness.toString());
+    startActivity(intent);
+    finish();
+
+  }
+
+
 }
--- front-end/android/app/src/main/java/ebaeapp/com/ebae/HistoryRestaurantActivity.java
+++ front-end/android/app/src/main/java/ebaeapp/com/ebae/HistoryRestaurantActivity.java
@@ -2,20 +2,68 @@ package ebaeapp.com.ebae;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static ebaeapp.com.ebae.R.id.restaurant_image;
+import static ebaeapp.com.ebae.R.id.restaurant_name;
+import static ebaeapp.com.ebae.R.id.restaurant_rating;
 
 /**
  * Created by Hung on 3/9/2017.
  */
 
 public class HistoryRestaurantActivity extends AppCompatActivity {
+    @BindView(R.id.reroll_button)
+    FloatingActionButton reroll_button;
+    @BindView(R.id.restaurant_image)
+    ImageView restaurant_image;
+    @BindView(R.id.restaurant_rating)
+    ImageView restaurant_rating;
+    @BindView(R.id.restaurant_name)
+    TextView restaurant_name;
+
+
+    JSONObject _businness;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
         ButterKnife.bind(this);
         Intent intent = getIntent();
+        try {
+            _businness = new JSONObject(getIntent().getStringExtra("business"));
+            updateActivity();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ViewGroup layout = (ViewGroup) reroll_button.getParent();
+        if(null!=layout) //for safety only  as you are doing onClick
+            layout.removeView(reroll_button);
+
+    }
+
+
+    private void updateActivity() throws JSONException {
+        Picasso.with(getApplicationContext())
+               .load(_businness.getString("image_url").replace("ms.jpg", "l.jpg"))
+               .into(restaurant_image);
+
+        Picasso.with(getApplicationContext())
+                .load(_businness.getString("rating_img_url"))
+                .into(restaurant_rating);
+
+        restaurant_name.setText(_businness.getString("name"));
     }
 }
